3955f82dcf59d367dc057fae0d58b45250b5b76a
developeryyt <yeontaey92@gmail.com>
2025-04-03 13:52:20 +0900

    api check

--- app/api/user/user.py
+++ app/api/user/user.py
@@ -2,8 +2,15 @@ from flask import Blueprint, request, jsonify
 
 check_auth = Blueprint('user', __name__)
 
-@check_auth.route('/auth', methods=['GET'])
+@check_auth.route('/check', methods=['GET'])
+def check_def():
+    return 'JUST CHECKING'
+
+
+@check_auth.route('/auth', methods=['POST'])
 def get_auth():
+    req_data = request.get_json()
     return jsonify({
-        "name" : "patrick"
+        "name" : req_data['username']
     })
+
